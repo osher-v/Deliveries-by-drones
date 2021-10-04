efbe4f7366527ff172587f1e6d8a37dc6cd1b862
avihendelsman <avihndlsmann@gmail.com>
2021-10-04 21:26:47 +0300

    add class

---
+++ Targil0/Program1239.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome1239()
+        {
+
+            Console.WriteLine("is any one else here?");
+            string name = Console.ReadLine();
+            Console.WriteLine("hii to you too {0}",name);
+
+            //Console.WriteLine(" מה עם שידוכים??????");
+        }
+    }
+}
