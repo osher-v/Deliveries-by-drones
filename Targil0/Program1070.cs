aa4909163c8299f18b8a8c267e6c7634a81a332b
osher-v <oshervaza1@gmail.com>
2021-10-04 20:53:16 +0300

    welcome func and initel code

---
+++ Targil0/Program1070.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1070();
+            Welcome1239();
+            Console.ReadKey();
+        }
+
+
+        static partial void Welcome1239();
+        private static void Welcome1070()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
