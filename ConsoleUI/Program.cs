585c6c32e082522af6f89a06c44e24d9f1818eb4
osher-v <oshervaza1@gmail.com>
2021-10-19 12:29:48 +0300

    12:29

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleUI
            Console.WriteLine(@"
 choose from the following options (type the selected number): 
 
-1.Insert options.
-2.Update options.
-3.Display options(singal).
-4.Display options (for the whole list).
-5.EXIT.
+1. Insert options.
+2. Update options.
+3. Display options(singel).
+4. Display options (for the whole list).
+5. EXIT.
 Your choice:");
             int choice = 0;
             int.TryParse( Console.ReadLine(),out choice);
@@ -31,52 +31,48 @@ Insert options:
 
 Your choice:");
                        int.TryParse(Console.ReadLine(), out choice);
-
                     break;
+
                 case 2:
                     Console.WriteLine(@"
-Insert options:
+Update options:
 
-1. Add a base station to the list of stations.
-2. Add a drone to the list of existing drones.
-3. Admission of a new customer to the customer list.
-4. Admission of package for shipment.
-
+1. Assigning a package to a drone
+2. Collection of a package by drone
+3. Delivery package to customer
+4. Sending a drone for charging at a base station
+5. Release drone from charging at base station
 Your choice:");
-
+                       int.TryParse(Console.ReadLine(), out choice);
                     break;
+
                 case 3:
                     Console.WriteLine(@"
-Insert options:
+Display options(singel):
 
-1. Add a base station to the list of stations.
-2. Add a drone to the list of existing drones.
-3. Admission of a new customer to the customer list.
-4. Admission of package for shipment.
+1. Base station view.
+2. Drone display.
+3. Customer view.
+4. Package view.
 
 Your choice:");
                     break;
+
                 case 4:
                     Console.WriteLine(@"
-Insert options:
+Display options (for the whole list):
 
-1. Add a base station to the list of stations.
-2. Add a drone to the list of existing drones.
-3. Admission of a new customer to the customer list.
-4. Admission of package for shipment.
+1. Displays a list of base stations.
+2. Displays the list of drone.
+3. View the customer list.
+4. Displays the list of packages.
+5. Displays a list of packages that have not yet been assigned to the drone.
+6. Display base stations with available charging stations.
 
 Your choice:");
                     break;
                 case 5:
-                    Console.WriteLine(@"
-Insert options:
-
-1. Add a base station to the list of stations.
-2. Add a drone to the list of existing drones.
-3. Admission of a new customer to the customer list.
-4. Admission of package for shipment.
-
-Your choice:");
+                    Console.WriteLine(@"Have a good day");
                     break;
                 default:
                     break;
