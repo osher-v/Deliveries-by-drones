9cc6ed67c74d48576fe1ee1fd1b500bf8c25ee68
osher-v <oshervaza1@gmail.com>
2021-10-20 12:21:03 +0300

    12:20

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,23 +2,130 @@
 
 namespace ConsoleUI
 {
-    enum Options {Insert=1,Update,Display,DisplayList,EXIT}
+    enum Options {Insert=1,Update, DisplaySingle, DisplayList,EXIT}
     class Program
     {
+          static public void InsertOptions()
+        {
+            int choice = 0;
+            Console.WriteLine(@"
+Insert options:
+
+1. Add a base station to the list of stations.
+2. Add a drone to the list of existing drones.
+3. Admission of a new customer to the customer list.
+4. Admission of package for shipment.
+
+Your choice:");
+            int.TryParse(Console.ReadLine(), out choice);
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+        }
+        static public void UpdateOptions()
+        {
+            int choice = 0;
+            Console.WriteLine(@"
+Update options:
+
+1. Assigning a package to a drone
+2. Collection of a package by drone
+3. Delivery package to customer
+4. Sending a drone for charging at a base station
+5. Release drone from charging at base station
+Your choice:");
+            int.TryParse(Console.ReadLine(), out choice);
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+        }
+        static public void DisplaySingleOptions()
+        {
+            int choice = 0;
+            Console.WriteLine(@"
+Display options(single):
+
+1. Base station view.
+2. Drone display.
+3. Customer view.
+4. Package view.
+
+Your choice:");
+            int.TryParse(Console.ReadLine(), out choice);
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+        }
+        static public void DisplayListOptions()
+        {
+            int choice = 0;
+            Console.WriteLine(@"
+Display options (for the whole list):
+
+1. Displays a list of base stations.
+2. Displays the list of drone.
+3. View the customer list.
+4. Displays the list of packages.
+5. Displays a list of packages that have not yet been assigned to the drone.
+6. Display base stations with available charging stations.
+
+Your choice:");
+            int.TryParse(Console.ReadLine(), out choice);
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                default:
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
             Options options;
-           
-            //DateTime time = new DateTime(2021, 10, 20);
-            //DateTime time1 = new DateTime(2021, 10, 20);
-            //Console.WriteLine(time.AddDays(5));
-            //time = time1.AddDays(5);
-            //Console.WriteLine();
+
 
             int choice = 0;
             do
             {
-                Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine(@"
 choose from the following options (type the selected number): 
 
@@ -34,111 +141,19 @@ Your choice:");
                 switch (options)
                 {
                     case Options.Insert:
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.WriteLine(@"
-Insert options:
-
-1. Add a base station to the list of stations.
-2. Add a drone to the list of existing drones.
-3. Admission of a new customer to the customer list.
-4. Admission of package for shipment.
-
-Your choice:");
-                        int.TryParse(Console.ReadLine(), out choice);
-                        switch (choice)
-                        {
-                            case 1:
-                                break;
-                            case 2:
-                                break;
-                            case 3:
-                                break;
-                            case 4:
-                                break;
-                            default:
-                                break;
-                        }
+                       InsertOptions();
                         break;
 
                     case Options.Update:
-                        //Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine(@"
-Update options:
-
-1. Assigning a package to a drone
-2. Collection of a package by drone
-3. Delivery package to customer
-4. Sending a drone for charging at a base station
-5. Release drone from charging at base station
-Your choice:");
-                        int.TryParse(Console.ReadLine(), out choice);
-                        switch (choice)
-                        {
-                            case 1:
-                                break;
-                            case 2:
-                                break;
-                            case 3:
-                                break;
-                            case 4:
-                                break;
-                            default:
-                                break;
-                        }
+                        UpdateOptions();
                         break;
 
-                    case Options.Display:
-                        Console.WriteLine(@"
-Display options(singel):
-
-1. Base station view.
-2. Drone display.
-3. Customer view.
-4. Package view.
-
-Your choice:");
-                        int.TryParse(Console.ReadLine(), out choice);
-                        switch (choice)
-                        {
-                            case 1:
-                                break;
-                            case 2:
-                                break;
-                            case 3:
-                                break;
-                            case 4:
-                                break;
-                            default:
-                                break;
-                        }
+                    case Options.DisplaySingle:
+                        DisplaySingleOptions();
                         break;
 
                     case Options.DisplayList:
-                        Console.WriteLine(@"
-Display options (for the whole list):
-
-1. Displays a list of base stations.
-2. Displays the list of drone.
-3. View the customer list.
-4. Displays the list of packages.
-5. Displays a list of packages that have not yet been assigned to the drone.
-6. Display base stations with available charging stations.
-
-Your choice:");
-                        int.TryParse(Console.ReadLine(), out choice);
-                        switch (choice)
-                        {
-                            case 1:
-                                break;
-                            case 2:
-                                break;
-                            case 3:
-                                break;
-                            case 4:
-                                break;
-                            default:
-                                break;
-                        }
+                        DisplayListOptions();
                         break;
                     case Options.EXIT:
                         Console.WriteLine("Have a good day");
