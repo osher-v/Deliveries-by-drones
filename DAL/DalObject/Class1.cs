6a1f6142f4143805f2ed8baf93349369936b14d7
osher-v <oshervaza1@gmail.com>
2021-10-23 22:12:09 +0300

    22:12

--- DAL/DalObject/Class1.cs
+++ DAL/DalObject/Class1.cs
@@ -7,36 +7,36 @@ using System.Threading.Tasks;
 using IDAL.DO;
 using System.ComponentModel;
 
-////////////////int index = baseStationsList.FindIndex(i => i.Id == 5);
-//exemmple//////BaseStation u = baseStationsList.Find(i => i.Id == 5);
-////////////////baseStationsList.ForEach(i => Console.WriteLine(i));
 namespace DalObject
 {
     /// <summary>
-    ///  
+    /// Contains boot data and list structure
     /// </summary>
     public static class DataSource
     {
+        /// <summary> A static Random that sets the random to select a millisecond to repel collisions </summary>
         internal static Random random = new Random(DateTime.Now.Millisecond);
-        //~~~~//
+        /// <summary> list of drones </summary>
         internal static List<Drone> droneList = new List<Drone>();
+        /// <summary> list of base stations </summary>
         internal static List<BaseStation> baseStationsList = new List<BaseStation>();
+        /// <summary>list of customers</summary>
         internal static List<Customer> customersList = new List<Customer>();
+        /// <summary> list of parcels </summary>
         internal static List<Parcel> parcelsList = new List<Parcel>();
+        /// <summary> class that responsible for counters </summary>
+        internal static List<DroneCharge> droneChargeList = new List<DroneCharge>();
 
-        /// <summary>
-        /// 
-        /// </summary>
         internal class Config
         {
+        /// <summary> Continuous number For the package </summary>
             public static int CountIdPackage = 0;
         }
-        /// <summary>
-        /// 
-        /// </summary>
+        /// <summary> responsible for initializing all entities </summary>
         public static void Initialize()
         {
-            //
+            // we chose to Initialize manual the Base Station
+            // becuse its just 2 and we want the landmarks of our homes
             baseStationsList.Add(new BaseStation {
                 Id = random.Next(100000000, 999999999),
                 StationName = "BnyBrak",
@@ -52,16 +52,16 @@ namespace DalObject
                 Longitude = 32.021679,
                 Latitude = 34.789990
             });
-
-            string[] modelNameArr = new string[5] { "I", "IX", "IIX", "VI", "IL" };
+            // We wanted a way to initialize types efficiently in a loop and chose to create an array of names from which to be randomly selected
+            string[] modelNameArr = new string[5] { "I", "IX", "IIX", "VI", "IL" }; 
             for (int i = 0; i < 5; i++)
             {
                 droneList.Add( new Drone {
                     Id = random.Next(100000000, 999999999),
                     Model = modelNameArr[i],
-                    MaxWeight = (WeightCategories)random.Next(0, 3),
+                    MaxWeight = (WeightCategories)random.Next(0, 3),//0=light,1=medium,2=heavy
                     Battery = random.Next(50, 100),
-                    Status = (DroneStatuses)random.Next(0, 3)
+                    Status = (DroneStatuses)random.Next(0, 3)//0=free, 1=inMaintenance, 2=busy
                 });
             }
 
@@ -73,8 +73,8 @@ namespace DalObject
                     Id = random.Next(100000000, 999999999),
                     Name = CustomersNameArr[i],
                     PhoneNumber = "0" + random.Next(50, 58) + "-" + random.Next(0000000, 9999999),
-                    Longitude = (float)((float)(random.NextDouble() * (33.3 - 31)) + 31),
-                    Latitude = (float)((float)(random.NextDouble() * (35.5 - 34.3)) + 34.3)
+                    Longitude = (float)((float)(random.NextDouble() * (33.3 - 31)) + 31),// get israel range 
+                    Latitude = (float)((float)(random.NextDouble() * (35.5 - 34.3)) + 34.3)//get israel range 
                 });
             }
 
@@ -83,16 +83,13 @@ namespace DalObject
             {
                 parcelsList.Add( new Parcel(){
                     Id = Config.CountIdPackage++,
-                    SenderId = customersList[random.Next(0, 5)].Id,
-                    TargetId = customersList[random.Next(5, 10)].Id,
-                    Weight = (WeightCategories)random.Next(0, 3),
-                    Priority = (Priorities)random.Next(0, 3),
+                    SenderId = customersList[random.Next(0, 5)].Id,// we choose from the list that we alrade have 
+                    TargetId = customersList[random.Next(5, 10)].Id,// we choose from the list that we alrade have  but not the same as before 
+                    Weight = (WeightCategories)random.Next(0, 3),//0=light,1=medium,2=heavy
+                    Priority = (Priorities)random.Next(0, 3),//0=regular, 1=fast, 2=urgent
                     DroneId = 0,
                     Requested = new DateTime(2021, 7, random.Next(1, 7))
                 });
-                //parcelArr[i].Assigned = parcelArr[i].Requested.AddHours(1);
-                //parcelArr[i].PickedUp = parcelArr[i].Assigned.AddHours(1);
-                //parcelArr[i].Delivered = parcelArr[i].PickedUp.AddHours(1);
             }
         }
     }
