3237af870825849492e46f7877984cdf56c86d98
avihendelsman <avihndlsmann@gmail.com>
2021-11-04 19:48:18 +0200

    19:48

---
+++ BL/BO/CustomerParent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        abstract class CustomerParent
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string PhoneNumber { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("the Id is {0,-5} \t the name is {1,-5} \t" +
+                    "the PhoneNumber is {2,-5}", Id, Name, PhoneNumber);
+            }
+
+        }
+    }  
+}
