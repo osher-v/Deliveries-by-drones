ca4c4b0063af2d7fdf65a9d318e0a71e9cf643b5
avihendelsman <avihndlsmann@gmail.com>
2021-11-04 13:43:58 +0200

    13:43

--- BL/BO/DeliveryByTransfer.cs
+++ BL/BO/DeliveryByTransfer.cs
@@ -11,8 +11,24 @@ namespace IBL
         class DeliveryByTransfer
         {
             public int Id { get; set; }
-            public int MyProperty { get; set; }
 
+            public WeightCategories Weight { get; set; }
+
+            public Priorities Prior { get; set; }
+
+            public bool MyProperty { get; set; }
+
+            public Location SourceLocation { get; set; }
+
+            public Location DestinationLocation { get; set; }
+
+            public double TransportDistance { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("     " , Id, Weight, Prior, MyProperty, SourceLocation, 
+                    DestinationLocation, TransportDistance);
+            }
         }
     }
 }
--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -4,25 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BL
+namespace IBL
 {
-    class Enums
+    namespace BO
     {
-        /// <summary>
-        ///  Enum to identify the order of priority
-        /// </summary>
-        public enum Priorities { regular, fast, urgent }
+        class Enums
+        {
+            /// <summary>
+            ///  Enum to identify the order of priority
+            /// </summary>
+            public enum Priorities { regular, fast, urgent }
 
-        /// <summary>
-        ///  Enum to identify the weight category
-        /// </summary>
-        public enum WeightCategories { light, medium, heavy }
-        
-        /// <summary>
-        ///  Enum to know the status
-        /// </summary>
-        public enum DroneStatuses { free, inMaintenance, busy }
-       
+            /// <summary>
+            ///  Enum to identify the weight category
+            /// </summary>
+            public enum WeightCategories { light, medium, heavy }
 
+            /// <summary>
+            ///  Enum to know the status
+            /// </summary>
+            public enum DroneStatuses { free, inMaintenance, busy }
+
+        }
     }
 }
