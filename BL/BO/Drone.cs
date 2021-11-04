263cec938cc4d8628c53b82fe15bf7c76ec48ed1
avihendelsman <avihndlsmann@gmail.com>
2021-11-04 22:15:05 +0200

    22:14

---
+++ BL/BO/Drone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        class Drone : DroneParent
+        {
+            public ParcelInDelivery Delivery { get; set; }
+
+            public override string ToString()
+            {
+                return base.ToString() + string.Format("the Delivery is {0,-5}", Delivery);
+            }
+        }
+    } 
+}
