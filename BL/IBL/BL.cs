881d79f44dd020a40984476707c8cba401a4b24b
avihendelsman <avihndlsmann@gmail.com>
2021-11-07 20:24:21 +0200

    20:24

---
+++ BL/IBL/BL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IBL
+{
+    public class BL
+    {
+        public BL()
+        {
+            IDal.IDal idal = new DalObject.DalObject();
+
+
+
+        }
+
+
+
+
+    }
+}
