94a24906f74132d560309a0150593d2ca11a01d2
osher-v <oshervaza1@gmail.com>
2021-11-11 19:36:29 +0200

    19:36

---
+++ BL/IBL/BL/BLbaseStation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBL.BO;
+
+namespace IBL
+{
+    public partial class BLbaseStation
+    {
+        public void AddStation( BaseStation newbaseStation)
+        {
+            if ((BaseStation.FindIndex(x => x.Id == newbaseStation.Id)) != -1)
+                throw new AddAnExistingObjectException();
+            DataSource.BaseStationsList.Add(newbaseStation);
+
+        }
+    }
+}
