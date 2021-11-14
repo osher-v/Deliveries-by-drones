2d0d46772210f1d2890a686f09459b33ba14d485
avihendelsman <avihndlsmann@gmail.com>
2021-11-14 13:02:04 +0200

    13:00

---
+++ BL/IBL/BL/BLcustomer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBL.BO;
+
+namespace IBL
+{
+    //public partial class BLcustomer
+    public partial class BL
+    {
+        public void AddCustomer(Customer customer)
+        {
+            IDAL.DO.Customer newCustomer = new IDAL.DO.Customer()
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                PhoneNumber = customer.PhoneNumber,
+                Longitude = customer.LocationOfCustomer.longitude,
+                Latitude = customer.LocationOfCustomer.latitude
+            };
+            try
+            {
+                AccessIdal.AddCustomer(newCustomer);
+            }
+            catch { }
+        }
+    }
+
+}
+
+
