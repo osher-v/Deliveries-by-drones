3f268c498e4983a374d3cd44c3f3ac59fc7b3ab2
osher-v <oshervaza1@gmail.com>
2021-11-28 20:15:45 +0200

    20:15

--- PL/DroneListWindow.xaml.cs
+++ PL/DroneListWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -24,11 +25,18 @@ namespace PL
     {
         public IBL.IBL AccessIbl;
         //public CancelEventArgs temp = new CancelEventArgs();
+        public ObservableCollection<IBL.BO.DroneToList> droneToLists;
         public DroneListWindow(IBL.IBL bl)
         {
             InitializeComponent();
             AccessIbl = bl;
-            DroneListView.ItemsSource = bl.GetDroneList();   
+            droneToLists = new ObservableCollection<DroneToList>();
+            foreach (var item in bl.GetDroneList())
+            {
+                droneToLists.Add(item);
+            }
+
+            DroneListView.ItemsSource = droneToLists;
             StatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatuses));
             WeightSelctor.ItemsSource = Enum.GetValues(typeof(WeightCategories));
         }
@@ -45,29 +53,29 @@ namespace PL
         private void StatusSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (WeightSelctor.SelectedItem == null)
-                DroneListView.ItemsSource = AccessIbl.GetDroneList(x => x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex);
+                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex);
             else
-                DroneListView.ItemsSource = AccessIbl.GetDroneList(x => x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex && x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex);
+                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex && x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex);
         }
 
         private void whigetSelctor_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (StatusSelector.SelectedItem == null)
-                DroneListView.ItemsSource = AccessIbl.GetDroneList(x => x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex);
+                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex);
             else
-                DroneListView.ItemsSource = AccessIbl.GetDroneList(x => x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex && x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex);
+                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex && x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex);
         }
 
         private void res_Click(object sender, RoutedEventArgs e)
         {
             StatusSelector.SelectedItem = null;
             WeightSelctor.SelectedItem = null;
-            DroneListView.ItemsSource = AccessIbl.GetDroneList();
+            DroneListView.ItemsSource = droneToLists;
         }
 
         private void BAddDrone_Click(object sender, RoutedEventArgs e)
         {
-            new DroneWindow(AccessIbl).Show();
+            new DroneWindow(AccessIbl, this, StatusSelector.SelectedItem, WeightSelctor.SelectedItem).Show();
         }
 
         private void Bclose_Click(object sender, RoutedEventArgs e)
--- PL/DroneWindow.xaml.cs
+++ PL/DroneWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,12 +24,126 @@ namespace PL
     public partial class DroneWindow : Window
     {
         public IBL.IBL AccessIbl;
-        public DroneWindow(IBL.IBL bl)
+
+        private DroneListWindow DroneListWindow;
+        public DroneWindow(IBL.IBL bl, DroneListWindow _DroneListWindow, DroneListWindow _StatusSelector, DroneListWindow _WeightSelctor)
         {
             InitializeComponent();
             AccessIbl = bl;
+            DroneListWindow = _DroneListWindow;
             TBWeight.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+            // SendToBl.IsEnabled = false;
+            BaseSTATION.ItemsSource = AccessIbl.GetBaseStationList();
+            //BaseStationID.ItemsSource==AccessIbl.GetBaseStationList().ToList();
+            BaseStationID.ItemsSource = AccessIbl.GetBaseStationList();
+            BaseStationID.DisplayMemberPath = "Id";
+
         }
 
+        private void Bclose_Click(object sender, RoutedEventArgs e)
+        {
+            DroneOperation.Close();
+        }
+
+        private void TBID_KeyDown(object sender, KeyEventArgs e)
+        {
+            /////////TBModel.Text = count.ToString();
+            // take only the kyes we alowed 
+            if (e.Key < Key.D0 || e.Key > Key.D9 )
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9) // we want keys from the num pud too
+                {
+                    e.Handled = true;
+                }
+                else
+                {
+                    e.Handled = false;
+                }
+            }      
+            if ( TBID.Text.Length > 8)
+                e.Handled = true;
+              
+        }
+
+        private void TBBaseStationForInitialCharging_KeyDown(object sender, KeyEventArgs e)
+        {
+            // take only the kyes we alowed 
+
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9) // we want keys from the num pud too
+                {
+                    e.Handled = true;
+                }
+                else
+                {
+                    e.Handled = false;
+                }
+            }
+            if (TBBaseStationForInitialCharging.Text.Length > 8)
+                e.Handled = true;
+
+        }
+
+        private void TBModel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (TBModel.Text.Length > 5)
+                e.Handled = true;
+        }
+
+        private void SendToBl_Click(object sender, RoutedEventArgs e)
+        {
+            if(TBModel.Text.Length != 0 && TBID.Text.Length != 0 && TBBaseStationForInitialCharging.Text.Length != 0 && TBWeight.SelectedItem!=null)
+            {
+                DroneToList newdrone = new DroneToList
+                {
+                    Id = int.Parse(TBID.Text), // כבר בדקנו שזה מספר על ידי זה שחסמנו את המקלדת 
+                    Model = TBModel.Text,
+                    MaxWeight = (WeightCategories)TBWeight.SelectedIndex,
+                };
+
+                try
+                {
+                    AccessIbl.AddDrone(newdrone, int.Parse(TBBaseStationForInitialCharging.Text));// כבר בדקנו שזה מספר על ידי זה שחסמנו את המקלדת 
+                    
+                   MessageBoxResult result= MessageBox.Show("The operation was successful", "info", MessageBoxButton.OK, MessageBoxImage.Information);
+                    switch (result)
+                    {
+                        case MessageBoxResult.OK:                          
+                            newdrone = AccessIbl.GetDroneList().ToList().Find(i => i.Id == newdrone.Id);
+                            DroneListWindow.droneToLists.Add(newdrone);
+                            DroneListWindow.DroneListView.Items.Refresh();
+                            
+
+                            Close();
+                            break;         
+                    }
+                }
+                catch (AddAnExistingObjectException ex)
+                {
+                    MessageBox.Show(ex.ToString(), "ERROR" ,MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (NonExistentObjectException ex)
+                {
+                    MessageBox.Show(ex.ToString(), "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (NoFreeChargingStations ex)
+                {
+                    MessageBox.Show(ex.ToString(), "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (NonExistentEnumException ex)
+                {
+                    MessageBox.Show(ex.ToString(), "ERROR" ,MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("נא ודאו שכל השדות מלאים","!שגיאה", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        
     }
 }
