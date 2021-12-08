fdd21409ebb7bcd385565786b71069750656072a
avihendelsman <avihndlsmann@gmail.com>
2021-12-08 11:41:38 +0200

    11:41

--- BL/BO/Others/Location.cs
+++ BL/BO/Others/Location.cs
@@ -15,12 +15,13 @@ namespace IBL
         {
             public double longitude { get; set; }
             public double latitude { get; set; }
-            public string LocationStringh { get; set; }
+            //public string LocationStringh { get; set; }
             public override string ToString()
             {
                 string convertLongitude = DalObject.DalObject.ConvertDecimalDegreesToSexagesimal(longitude, (IDAL.DO.LongitudeAndLatitude)1);
                 string convertLatitude = DalObject.DalObject.ConvertDecimalDegreesToSexagesimal(latitude, (IDAL.DO.LongitudeAndLatitude)2);
-                return string.Format(" longitude is: {0,-8} \t  latitude is {1,-8} ", convertLongitude, convertLatitude);
+                //return string.Format(" longitude is: {0,-8} \t  latitude is {1,-8} ", convertLongitude, convertLatitude);
+                return string.Format("{0,-8} \t {1,-8} ", convertLongitude, convertLatitude);
             }
 
         }
