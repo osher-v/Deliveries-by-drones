fbf1b7036a5b94f288c363c575aa7dc60031b3bc
avihendelsman <avihndlsmann@gmail.com>
2021-12-19 12:59:46 +0200

    13:00

---
+++ PL/BaseStationWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for BaseStationWindow.xaml
+    /// </summary>
+    public partial class BaseStationWindow : Window
+    {
+        /// <summary>
+        /// update constractor
+        /// </summary>
+        /// <param name="bl"></param>
+        /// <param name="_DroneListWindow"></param>
+        /// <param name="BaseStationTo"></param>
+        /// <param name="_indexDrone"></param>
+        public BaseStationWindow(BlApi.IBL bl, ListView _DroneListWindow, BaseStationsToList BaseStationTo, int _indexBaseStation)
+        {
+            InitializeComponent();
+        }
+    }
+}
--- PL/ListView.xaml.cs
+++ PL/ListView.xaml.cs
@@ -24,8 +24,11 @@ namespace PL
     {
         public BlApi.IBL AccessIbl = BlApi.BlFactory.GetBL();
 
-        /// <summary> crate a observab list of type IBL.BO.DroneToList (to see changes in live) </summary>
+        /// <summary> crate a observabs list of type IBL.BO.object (to see changes in live) </summary>
         public ObservableCollection<BO.DroneToList> droneToLists;
+        public ObservableCollection<BO.BaseStationsToList> BaseStationToLists;
+        public ObservableCollection<BO.CustomerToList> CustomerToLists;
+        public ObservableCollection<BO.ParcelToList> ParcelToLists;
 
         /// <summary> a bool to help us disable the x bootum  </summary>
         public bool ClosingWindow { get; private set; } = true;
@@ -40,9 +43,10 @@ namespace PL
 
             //AccessIbl = bl;
 
-            //craet observer and set the list accordale to ibl drone list 
+            #region drone Observable and listOfDrones
+            //craet observer and set the list accordale to ibl drone list.
             droneToLists = new ObservableCollection<DroneToList>();
-            List<BO.DroneToList> drones = AccessIbl.GetDroneList().ToList();
+            List<BO.DroneToList> drones = AccessIbl.GetDroneList().ToList();//למה העברנו את זה ככה??
             foreach (var item in drones)
             {
                 droneToLists.Add(item);
@@ -53,6 +57,55 @@ namespace PL
 
             //display the defult list 
             listOfDrones.ItemsSource = droneToLists;
+            #endregion drone Observable and listOfDrones
+
+            #region baseStations Observable and listOfBaseStations
+            //craet observer and set the list accordale to ibl BaseStation list. 
+            BaseStationToLists = new ObservableCollection<BaseStationsToList>();
+            List<BO.BaseStationsToList> baseStations = AccessIbl.GetBaseStationList().ToList();//למה העברנו את זה ככה??
+            foreach (var item in baseStations)
+            {
+                BaseStationToLists.Add(item);
+            }
+
+            //new event that will call evre time that the ObservableCollection didact a change 
+            //BaseStationToLists.CollectionChanged += BaseStationToLists_CollectionChanged;
+
+            //display the defult list 
+            listOfBaseStations.ItemsSource = BaseStationToLists;
+            #endregion baseStations Observable and listOfBaseStations
+
+            #region customer Observable and listOfCustomer
+            //craet observer and set the list accordale to ibl BaseStation list. 
+            CustomerToLists = new ObservableCollection<CustomerToList>();
+            List<BO.CustomerToList> customer = AccessIbl.GetCustomerList().ToList();//למה העברנו את זה ככה??
+            foreach (var item in customer)
+            {
+                CustomerToLists.Add(item);
+            }
+
+            //new event that will call evre time that the ObservableCollection didact a change 
+            //BaseStationToLists.CollectionChanged += BaseStationToLists_CollectionChanged;
+
+            //display the defult list 
+            listOfCustomers.ItemsSource = CustomerToLists;
+            #endregion customer Observable and listOfCustomer
+
+            #region parcel Observable and listOfParcel
+            //craet observer and set the list accordale to ibl BaseStation list. 
+            ParcelToLists = new ObservableCollection<ParcelToList>();
+            List<BO.ParcelToList> parcel = AccessIbl.GetParcelList().ToList();//למה העברנו את זה ככה??
+            foreach (var item in parcel)
+            {
+                ParcelToLists.Add(item);
+            }
+
+            //new event that will call evre time that the ObservableCollection didact a change 
+            //BaseStationToLists.CollectionChanged += BaseStationToLists_CollectionChanged;
+
+            //display the defult list 
+            listOfParcels.ItemsSource = ParcelToLists;
+            #endregion parcel Observable and listOfParcel
 
             //combobox of dronesList handling.
             CBStatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatuses));
@@ -69,11 +122,10 @@ namespace PL
             StatusSelectorChanged();
         }
 
-        #region combobox of dronesList handling
         /// <summary>
         /// help fanction to choose what to show on the user side accordingly to user cohises ( bonous)
         /// </summary>
-        public void StatusSelectorChanged() //אולי לעשות סוויץ
+        public void StatusSelectorChanged()
         {
             if (CBWeightSelctor.SelectedItem == null && CBStatusSelector.SelectedItem == null)
             {
@@ -93,6 +145,7 @@ namespace PL
             }
         }
 
+        #region combobox of dronesList handling
         /// <summary>
         /// show on the user side accordingly to user cohises
         /// </summary>
@@ -145,7 +198,7 @@ namespace PL
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void lists_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void listOfDrones_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             DroneToList drone = (DroneToList)listOfDrones.SelectedItem;
             if (drone != null)// if the user click on empty space in the view list we donr open anything
@@ -154,6 +207,86 @@ namespace PL
                 this.IsEnabled = false; // to privent anotur click on the list window chosse we donr want alot of windows togter.
                 new DroneWindow(AccessIbl, this, drone, indexDrone).Show();//open the drone windowon acction
             }
+        }
+
+
+
+        /// <summary>
+        /// open BaseStation window in acction when didect double clik
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listOfBaseStations_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            BaseStationsToList baseStations = (BaseStationsToList)listOfBaseStations.SelectedItem;
+            if (baseStations != null)// if the user click on empty space in the view list we donr open anything
+            {
+                int indexBaseStation = listOfBaseStations.SelectedIndex;
+                //this.IsEnabled = false; // to privent anotur click on the list window chosse we donr want alot of windows togter.
+                new BaseStationWindow(AccessIbl, this, baseStations, indexBaseStation).Show();//open the drone windowon acction
+            }
+        }
+
+        /// <summary>
+        /// open Customer window in acction when didect double clik
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listOfCustomers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            CustomerToList customer = (CustomerToList)listOfCustomers.SelectedItem;
+            if (customer != null)// if the user click on empty space in the view list we donr open anything
+            {
+                int indexCustomer = listOfCustomers.SelectedIndex;
+                //this.IsEnabled = false; // to privent anotur click on the list window chosse we donr want alot of windows togter.
+                new CustomerWindow(AccessIbl, this, customer, indexCustomer).Show();//open the drone windowon acction
+            }
+        }
+
+        /// <summary>
+        /// open Parcel window in acction when didect double clik
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listOfParcels_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ParcelToList parcel = (ParcelToList)listOfParcels.SelectedItem;
+            if (parcel != null)// if the user click on empty space in the view list we donr open anything
+            {
+                int indexParcel = listOfParcels.SelectedIndex;
+                //this.IsEnabled = false; // to privent anotur click on the list window chosse we donr want alot of windows togter.
+                new ParcelWindow(AccessIbl, this, parcel, indexParcel).Show();//open the drone windowon acction
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Add drone button which activates the builder in the Add drone window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // we send ""this"" window becuse we want to use it in the new window
+            new DroneWindow(AccessIbl, this).Show();
+            this.IsEnabled = false;
+            //switch (switch_on)
+            //{
+            //    default:
+            //}
         }
 
         #region close
@@ -176,191 +309,9 @@ namespace PL
             ClosingWindow = false; // we alowd the close option
             Close();
         }
-        #endregion close
+        #endregion close     
 
-        /// <summary>
-        /// Add drone button which activates the builder in the Add drone window
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void BAdd_Click(object sender, RoutedEventArgs e)
-        {
-            // we send ""this"" window becuse we want to use it in the new window
-            new DroneWindow(AccessIbl, this).Show();
-            this.IsEnabled = false;
-        }
     }
 }
 
-
-//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.ComponentModel;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Shapes;
-//using BO;
-
-//namespace PL
-//{
-//    /// <summary>
-//    /// Interaction  logic for DroneListWindow.xaml
-//    /// </summary>
-//    public partial class DroneListWindow : Window
-//    {
-//        #region drone list view
-//        public BlApi.IBL AccessIbl;
-
-//        /// <summary> crate a observab list of type IBL.BO.DroneToList (to see changes in live) </summary>
-//        public ObservableCollection<BO.DroneToList> droneToLists;
-
-//        /// <summary> a bool to help us disable the x bootum  </summary>
-//        public bool ClosingWindow { get; private set; } = true;
-
-//        /// <summary>
-//        /// constractor for the DroneListWindow that will start the InitializeComponent ans fill the Observable Collection
-//        /// </summary>
-//        /// <param name="bl">get AccessIbl from main win</param>
-//        public DroneListWindow(BlApi.IBL bl)
-//        {
-//            InitializeComponent();
-//            AccessIbl = bl;
-//            //craet observer and set the list accordale to ibl drone list 
-//            droneToLists = new ObservableCollection<DroneToList>();
-//            List<BO.DroneToList> drones = bl.GetDroneList().ToList();
-//            foreach (var item in drones)
-//            {
-//                droneToLists.Add(item);
-//            }
-//            //new event that will call evre time that the ObservableCollection didact a change 
-//            droneToLists.CollectionChanged += DroneToLists_CollectionChanged;
-//            //display the defult list 
-//            DroneListView.ItemsSource = droneToLists;
-//            StatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatuses));
-//            WeightSelctor.ItemsSource = Enum.GetValues(typeof(WeightCategories));
-//        }
-
-//        /// <summary>
-//        /// a new event that we crate in the intaklizer :DroneToLists_CollectionChanged:
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void DroneToLists_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-//        {
-//            StatusSelectorChanged();
-//        }
-
-//        /// <summary>
-//        /// help fanction to choose what to show on the user side accordingly to user cohises ( bonous)
-//        /// </summary>
-//        public void StatusSelectorChanged()
-//        {
-//            if (WeightSelctor.SelectedItem == null && StatusSelector.SelectedItem == null)
-//            {
-//                DroneListView.ItemsSource = droneToLists.ToList();
-//            }
-//            else if (WeightSelctor.SelectedItem == null)
-//            {
-//                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex);
-//            }
-//            else if (StatusSelector.SelectedItem == null)
-//            {
-//                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex);
-//            }
-//            else
-//            {
-//                DroneListView.ItemsSource = droneToLists.ToList().FindAll(x => x.Statuses == (DroneStatuses)StatusSelector.SelectedIndex && x.MaxWeight == (WeightCategories)WeightSelctor.SelectedIndex);
-//            }
-//        }
-
-//        /// <summary>
-//        /// show on the user side accordingly to user cohises
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void StatusSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-//        {
-//            StatusSelectorChanged();
-//        }
-
-//        /// <summary>
-//        /// show on the user side accordingly to user cohises
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void weightSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-//        {
-//            StatusSelectorChanged();     
-//        }
-
-//        /// <summary>
-//        /// restart modem to set all options to default
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void res_Click(object sender, RoutedEventArgs e)
-//        {
-//            StatusSelector.SelectedItem = null;
-//            WeightSelctor.SelectedItem = null;
-//            StatusSelectorChanged();
-//        }
-
-//        /// <summary>
-//        /// Add drone button which activates the builder in the Add drone window
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void BAddDrone_Click(object sender, RoutedEventArgs e)
-//        {
-//            // we send ""this"" window becuse we want to use it in the new window
-//            new DroneWindow(AccessIbl, this).Show();
-//            this.IsEnabled = false;
-//        }
-
-//        /// <summary>
-//        /// cancel the option to clik x to close the window 
-//        /// </summary>
-//        /// <param name="e">close window</param>
-//        protected override void OnClosing(CancelEventArgs e)
-//        {
-//            e.Cancel = ClosingWindow;
-//        }
-
-//        /// <summary>
-//        /// to aloow closing again but just in the spcific close boutoon 
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void Bclose_Click(object sender, RoutedEventArgs e)
-//        {
-//            ClosingWindow = false; // we alowd the close option
-//            Close();
-//        }
-//        /// <summary>
-//        /// open drone window in acction when didect double clik
-//        /// </summary>
-//        /// <param name="sender"></param>
-//        /// <param name="e"></param>
-//        private void DroneListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-//        {
-//            DroneToList drone = (DroneToList)DroneListView.SelectedItem;
-//            if (drone != null)// if the user click on empty space in the view list we donr open anything
-//            {
-//                int indexDrone = DroneListView.SelectedIndex;
-//                this.IsEnabled = false; // to privent anotur click on the list window chosse we donr want alot of windows togter.
-//                new DroneWindow(AccessIbl, this,drone, indexDrone).Show();//open the drone windowon acction
-//            }
-//        }
-//        #endregion
-//    }
-//}
 
