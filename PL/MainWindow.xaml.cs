83be7cc16041eb19c5d9e0da185a289e4ff35d48
osher-v <oshervaza1@gmail.com>
2022-01-12 12:19:10 +0200

    12:20

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ using System.ComponentModel;
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml 
     /// </summary>
     public partial class MainWindow : Window
     {
